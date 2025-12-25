9d2ad31eb4ce561b39bc1a2d7e22f0576cd056b4
kchern <kchern@dwavesys.com>
2025-12-25 06:46:35 +0000

    Add function to execute visually-selected code

--- init.lua
+++ init.lua
@@ -1033,6 +1033,7 @@ require('lazy').setup({
       dap.listeners.before.event_exited.dapui_config = function()
         dapui.close()
       end
+
       vim.keymap.set('n', '<F5>', function()
         dap.continue()
       end, { desc = 'Debug Continue' })
@@ -1128,6 +1129,56 @@ require('lazy').setup({
     },
   },
 })
+
+--- Return the visually selected text as an array with an entry for each line
+---
+--- @return string[]|nil lines The selected text as an array of lines.
+function get_visual_selection_text()
+  local _, srow, scol = unpack(vim.fn.getpos('v'))
+  local _, erow, ecol = unpack(vim.fn.getpos('.'))
+
+  -- visual line mode
+  if vim.fn.mode() == 'V' then
+    if srow > erow then
+      return vim.api.nvim_buf_get_lines(0, erow - 1, srow, true)
+    else
+      return vim.api.nvim_buf_get_lines(0, srow - 1, erow, true)
+    end
+  end
+
+  -- regular visual mode
+  if vim.fn.mode() == 'v' then
+    if srow < erow or (srow == erow and scol <= ecol) then
+      return vim.api.nvim_buf_get_text(0, srow - 1, scol - 1, erow - 1, ecol, {})
+    else
+      return vim.api.nvim_buf_get_text(0, erow - 1, ecol - 1, srow - 1, scol, {})
+    end
+  end
+
+  -- visual block mode
+  if vim.fn.mode() == '\22' then
+    local lines = {}
+    if srow > erow then
+      srow, erow = erow, srow
+
+    end
+    if scol > ecol then
+      scol, ecol = ecol, scol
+    end
+    for i = srow, erow do
+      table.insert(
+        lines,
+        vim.api.nvim_buf_get_text(0, i - 1, math.min(scol - 1, ecol), i - 1, math.max(scol - 1, ecol), {})[1]
+      )
+    end
+    return lines
+  end
+
+end
+
+vim.keymap.set("v", "<leader>de", function()
+  require("dap").repl.execute(table.concat(get_visual_selection_text(), "\n"))
+end)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
